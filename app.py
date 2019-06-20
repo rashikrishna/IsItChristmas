d8208cbf2aa5e0e7897914b87d7fc59e1619c7ec
rashi <rashikrishna16@gmail.com>
2019-06-20 15:47:48 +0530

    Jija2

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     now=datetime.datetime.now()
-    christmas=now.month==12 and now.day==25
-    return render_template("/home/rashi/Christmas/index.html",christmas=christmas)
+    christmas= now.month==12 and now.day==25
+    return render_template("index.html",christmas=christmas)
 
 if (__name__=="__main__"):
      app.run(port=5001)
